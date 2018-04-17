ae076109e4cd27ce6c8a690f8c6c49c21836a912
Ina <inch.binch@gmail.com>
2018-04-17 20:09:06 +0200

    Add POST and GET methods to the /search4 route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from functions import search4letters
 
 app = Flask(__name__)
@@ -8,13 +8,18 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-@app.route('/search4')
+
+@app.route('/search4', methods=['POST', 'GET'])
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything!', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    return str(search4letters(phrase, letters))
+
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to the web search for letters app!')
+
 
 if __name__ == '__main__':
     app.run()
