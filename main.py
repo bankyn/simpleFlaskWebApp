f47b4b1409e49ea601df310d39919545e156fdc6
Ina <inch.binch@gmail.com>
2018-04-17 19:52:06 +0200

    Add new Pages/URLs using Jinja Templates

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from functions import search4letters
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World!'
 
 @app.route('/search4')
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything!', 'eiru,!'))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to the web search for letters app!')
 
 if __name__ == '__main__':
     app.run()
