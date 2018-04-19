485d5934712063943697382362eed9b0a95c734b
Ina <inch.binch@gmail.com>
2018-04-19 17:27:22 +0200

    Enter debug mode. Test and improve app

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from functions import search4letters
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return redirect('/entry')
-
-
 @app.route('/search4', methods=['POST', 'GET'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
@@ -16,14 +11,15 @@ def do_search() -> 'html':
     results = str(search4letters(phrase, letters))
     title = 'Letter Search Results'
 
-    return render_template('results.html', the_title = title, the_results = results, the_letters = letters, the_phrase = phrase)
+    return render_template('results.html', the_title=title, the_results=results, the_letters=letters, the_phrase=phrase)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to the web search for letters app!')
 
 
 if __name__ == '__main__':
-    app.run() # flask defaults: host 127.0.0.1 port 5000
+    app.run(debug=True)  # flask defaults: host 127.0.0.1 port 5000
     # app.run(host='0.0.0.0', port=80)
