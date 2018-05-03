0308e77fdde6627cd544dec69f93c744012d00f9
Ina <inch.binch@gmail.com>
2018-05-03 14:16:15 +0200

    test the log file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, escape
 from functions import search4letters
 
 app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('lsearch.log', 'a') as log:
-        print(req, res, file=log)
-
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST', 'GET'])
 def do_search() -> 'html':
@@ -23,11 +22,21 @@ def do_search() -> 'html':
 
     return render_template('results.html', the_title=title, the_results=results, the_letters=letters, the_phrase=phrase)
 
-
 @app.route('/')
 @app.route('/entry', methods=['POST', 'GET'])
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to the search for letters app!')
+
+@app.route('/viewlog', methods=['POST'])
+def view_the_log() -> str:
+    with open('lsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
+
+@app.route('/clearlog', methods=['POST'])
+def clear_the_log():
+    open('lsearch.log', 'w').close()
+    return redirect('/', code=302)
 
 if __name__ == '__main__':
     app.run(debug=True)  # flask defaults: (host='127.0.0.1', port=5000)
